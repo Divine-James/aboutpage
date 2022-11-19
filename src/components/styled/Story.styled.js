21b9244ef8254330064df10badc974a84bc980a8
Divine <104062206+Divine-James@users.noreply.github.com>
2022-11-19 22:49:58 +0100

    mbs

--- src/components/styled/Story.styled.js
+++ src/components/styled/Story.styled.js
@@ -4,7 +4,7 @@ export const StyledStory = styled.div`
   justify-content: space-between;
   padding: 40px 50px;
   background-color: #eef1fc;
-  width: 1440px;
+  width: 1040px;
   @media (max-width: 375px) {
     display: flex;
     flex-direction: column;
