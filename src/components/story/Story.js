433cef818afdb9734f9f268a6c4bd19e103cf9f2
Divine <104062206+Divine-James@users.noreply.github.com>
2022-11-20 00:49:57 +0100

    change

--- src/components/story/Story.js
+++ src/components/story/Story.js
@@ -14,16 +14,16 @@ export default function Story() {
           <StoryHead>Our Story</StoryHead>
           <StoryP>
             Founded in November 2022 during an internship program of great
-            minds,the founder took note of the rising trend: high profile
-            individual and business are becoming more susceptible to negative
-            review, where a few words could have a devastating impact on the
-            reputation, sales, recruitment efforts of the victim. for this
-            reason, the fixit vision came to life.
+            minds, the founder took note of a rising trend: high profile
+            individuals and businesses are becoming more susceptible to negative
+            reviews, where a few words could have a devastating impact on the
+            reputation, sales, and recruitment efforts of the victim. For this
+            reason, the FixIt vision came to life.
           </StoryP>
           <StoryP>
-            At fixit, we aim to provide a stellar reputation management service
-            by utlising the lastest technology and the most effective user
-            experience processes
+            At FixIt, we aim to provide a stellar reputation management service
+            by utilising the latest technology, and the most effective user
+            experience processes.
           </StoryP>
         </StyledStorytxt>
         <StoryImg
